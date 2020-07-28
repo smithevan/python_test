d4c5544725c82055ec0084c4943fdfc2b04a13b1
Evan Smith <smith.m.evan@gmail.com>
2020-07-28 21:22:23 +0100

    iterating over and picking numbers into new array

--- lottery_pick.py
+++ lottery_pick.py
@@ -1,12 +1,17 @@
-from random import * 
+from random import randint 
 
 lottery_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+picked_numbers = []
 
-iterator = randint(1, 25)
 #print (iterator)
 for number in lottery_numbers: 
+    iterator = randint(1, len(lottery_numbers))
     if number == iterator: 
-        print (iterator) 
+        picked_numbers.append(number)
         lottery_numbers.remove(number)
+    
+
+for number in picked_numbers:
+    print (number)
     
     
