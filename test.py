81864c413c16b235c56d480012b9b41091421243
Evan Smith <smith.m.evan@gmail.com>
2020-07-28 19:22:18 +0100

    add to 10 unless 100

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-#number = input("Input Number ")
-number = 5
+number = 1
+while number != 100: 
+    number = int(input())
 
-def count_to_ten(number):
-    while number <= 10:
-        print (number)
-        number += 1
+    def count_to_ten(number):
+        while number < 10:
+            number += 1
+            print (number)
+        print (" ")
 
-count_to_ten(number)
+    count_to_ten(number)
