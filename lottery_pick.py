e96be75b3d5775f08624fc04fce92f7497516b79
Evan Smith <smith.m.evan@gmail.com>
2020-07-28 21:32:09 +0100

    picks numbers at random from array list and transfers them to new array, stopping at 6

--- lottery_pick.py
+++ lottery_pick.py
@@ -3,12 +3,12 @@ from random import randint
 lottery_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 picked_numbers = []
 
-#print (iterator)
-for number in lottery_numbers: 
-    iterator = randint(1, len(lottery_numbers))
-    if number == iterator: 
-        picked_numbers.append(number)
-        lottery_numbers.remove(number)
+while len(picked_numbers) < 6:
+    for number in lottery_numbers: 
+        iterator = randint(1, len(lottery_numbers))
+        if number == iterator: 
+            picked_numbers.append(number)
+            lottery_numbers.remove(number)
     
 
 for number in picked_numbers:
