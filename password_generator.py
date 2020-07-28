929f5ce909bb0dff0dd1cf1b2906008181f2281f
Evan Smith <smith.m.evan@gmail.com>
2020-07-28 23:15:05 +0100

    randomized the pw lenght

--- password_generator.py
+++ password_generator.py
@@ -3,7 +3,7 @@ from random import randint
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 password = []
 
-while len(password) < 6: 
+while len(password) < randint(6, 15): 
     letter_or_number = randint(0, 1)
     capital_true_false = randint(0, 1)
     if letter_or_number == 0:  
