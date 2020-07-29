56c25dd9a808715ac26f5cc3af3ab1ea96944761
Evan Smith <smith.m.evan@gmail.com>
2020-07-29 17:43:35 +0100

    recognising words, even if there are spaces in stange places

--- word_recognition.py
+++ word_recognition.py
@@ -1,11 +1,20 @@
 user_input = input("Type a Sentence:")
 wordcount = 0
 
-for character in user_input: 
+
+for i, character in enumerate(user_input): 
+
     if character == " ": 
         wordcount += 1 
-    if character == user_input[len(user_input) -1]:
-        wordcount += 1
 
-print(wordcount)
+    if not i:
+        wordcount += 1
+    
+    if character == user_input[len(user_input) -1] and character == " ":
+        wordcount -= 1
+
+if wordcount == 1:
+    print("I see " + str(wordcount) + " word")
+else:
+    print("I see " + str(wordcount) + " words")
 
