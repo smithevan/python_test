283433c7e2abdaa36e5178498c30f90105a39eb4
Evan Smith <smith.m.evan@gmail.com>
2020-08-01 15:17:20 +0100

    trying out pi from rand int problem example

--- word_recognition.py
+++ word_recognition.py
@@ -4,14 +4,17 @@ wordcount = 0
 
 for i, character in enumerate(user_input): 
 
-    if character == " ": 
-        wordcount += 1 
+        if character == " ": 
+            wordcount += 1 
 
-    if not i:
-        wordcount += 1
+        if not i:
+            wordcount += 1
     
-    if character == user_input[len(user_input) -1] and character == " ":
-        wordcount -= 1
+        if character == user_input[len(user_input) -1] and character == " ":
+            wordcount -= 1
+    
+   
+       
 
 if wordcount == 1:
     print("I see " + str(wordcount) + " word")
