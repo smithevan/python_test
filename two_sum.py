b4a85aa85aba0f7e2d31b8b7ec21cae88b5984bc
Evan Smith <smith.m.evan@gmail.com>
2020-08-01 20:47:57 +0100

    new approach

--- two_sum.py
+++ two_sum.py
@@ -8,16 +8,29 @@ target = 10
 def two_sum(array, target):
     first_number = 0
     second_number = 0
-    for array_element in array: 
-        while first_number + second_number != target: 
-            if first_number == 0 and second_number == 0: 
-                first_number = array[array_element]
-            elif first_number != 0 and second_number == 0:
-                second_number = array[array_element] 
-            elif first_number != 0 and second_number != 0:
-                first_number = second_number
-                second_number = array[array_element]
-            return [array_element - 1, array_element + 1]
+    for x in array: 
+        first_number = array[x]
+        first_number_index = x
+        for y in array: 
+            second_number = array[y]
+            second_number_index = y
+            if first_number + second_number == target:
+                return [first_number_index, second_number_index]
+
+    
+    
+    
+   
+    #for array_element in array: 
+        #while first_number + second_number != target: 
+            #if first_number == 0 and second_number == 0: 
+             #   first_number = array[array_element]
+    #        elif first_number != 0 and second_number == 0:
+    #            second_number = array[array_element] 
+    #        elif first_number != 0 and second_number != 0:
+    #            first_number = second_number
+    #            second_number = array[array_element]
+    #        return [array_element - 1, array_element + 1]
 
 print(two_sum(array_set, target))
         
