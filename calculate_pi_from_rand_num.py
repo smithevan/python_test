f5bb15de196cac96b88213daf8c75a01834e68ce
Evan Smith <smith.m.evan@gmail.com>
2020-08-01 15:49:06 +0100

    model works to move closer to 3.14159, etc as more points added

--- calculate_pi_from_rand_num.py
+++ calculate_pi_from_rand_num.py
@@ -1,18 +1,23 @@
 #Given a random set of numbers, determine pi: following along with example on youtube
-import from math sqrt
+import math
+import random 
+
 
 def estimate_pi(points):
     points_in_circle = 0
     points_in_square = 0
-    for i in range(points):
-        x = random.random(0, 1)
-        y = random.random(0, 1)
-        distance_point_to_origin = sqrt(x**2 + y**2)
+    for _ in range(points):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        distance_point_to_origin = math.sqrt(x**2 + y**2)
         if distance_point_to_origin <= 1:
             points_in_circle += 1
         points_in_square += 1
 
-    return 4 * points_in_circle/points_in_square
+    result = 4 * points_in_circle/points_in_square
+    return result
 
+sample_set = int(input('Input Number:'))
+print(estimate_pi(sample_set))
 
 
