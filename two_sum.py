c8f133280f5c56e994b2fdfa2be62f2a7635d999
Evan Smith <smith.m.evan@gmail.com>
2020-08-01 20:12:09 +0100

    two_sum finds if two numbers sum to the target and reports back the result

---
+++ two_sum.py
@@ -0,0 +1,24 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+array_set = [1, 5, 9, 34] #target 10
+target = 10
+
+def two_sum(array, target):
+    first_number = 0
+    second_number = 0
+    for array_element in array: 
+        while first_number + second_number != target: 
+            if first_number == 0 and second_number == 0: 
+                first_number = array[array_element]
+            elif first_number != 0 and second_number == 0:
+                second_number = array[array_element] 
+            elif first_number != 0 and second_number != 0:
+                first_number = second_number
+                second_number = array[array_element]
+    return first_number + second_number
+
+print(two_sum(array_set, target))
+        
+        
