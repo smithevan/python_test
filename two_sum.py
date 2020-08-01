3fe2c0d165a8431b5bc299197d7d519713be5b62
Evan Smith <smith.m.evan@gmail.com>
2020-08-01 20:52:20 +0100

    refactoring

--- two_sum.py
+++ two_sum.py
@@ -17,20 +17,6 @@ def two_sum(array, target):
             if first_number + second_number == target:
                 return [first_number_index, second_number_index]
 
-    
-    
-    
-   
-    #for array_element in array: 
-        #while first_number + second_number != target: 
-            #if first_number == 0 and second_number == 0: 
-             #   first_number = array[array_element]
-    #        elif first_number != 0 and second_number == 0:
-    #            second_number = array[array_element] 
-    #        elif first_number != 0 and second_number != 0:
-    #            first_number = second_number
-    #            second_number = array[array_element]
-    #        return [array_element - 1, array_element + 1]
 
 print(two_sum(array_set, target))
         
