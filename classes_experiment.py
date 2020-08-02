c365e6330ec783dc024758147060007595799813
Evan Smith <smith.m.evan@gmail.com>
2020-08-02 16:40:25 +0100

    able to use abstract classes and inheretence to create vehicle class with speed and name for all sub classes of vehicles

--- classes_experiment.py
+++ classes_experiment.py
@@ -1,15 +1,31 @@
-class Plane:
+class Vehicle: 
+
+    def __init__(self, name, speed): 
+        self.name = name
+        self.speed = speed
+    
+    def get_name(self):
+        print(self.name)
+    
+    def get_speed(self):
+        print(self.speed)
+
+class Plane(Vehicle):
 
     def fly(self):
         print ("I'm Flying")
 
-class Car: 
+class Car(Vehicle): 
 
     def drive(self):
         print ("I'm driving")
 
-ba747 = Plane()
+ba747 = Plane("747", 800)
 ba747.fly()
+ba747.get_name()
+ba747.get_speed()
 
-ford_fiesta = Car()
+ford_fiesta = Car("Ford Fiesta", 80)
 ford_fiesta.drive()
+ford_fiesta.get_name()
+ford_fiesta.get_speed()
