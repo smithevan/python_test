af82b3a1ef0e03c051ec6715425f02e7e5bfa5f1
Evan Smith <smith.m.evan@gmail.com>
2020-08-02 16:34:20 +0100

    able to drive cars and fly planes

--- classes_experiment.py
+++ classes_experiment.py
@@ -3,5 +3,13 @@ class Plane:
     def fly(self):
         print ("I'm Flying")
 
-BA747 = Plane()
-BA747.fly()
+class Car: 
+
+    def drive(self):
+        print ("I'm driving")
+
+ba747 = Plane()
+ba747.fly()
+
+ford_fiesta = Car()
+ford_fiesta.drive()
